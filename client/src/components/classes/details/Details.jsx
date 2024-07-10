41486a33fac6dd6fade25dd4f83347b70a5080ab
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-07-10 16:10:59 +0300

    remove duplicated request to the server

--- client/src/components/classes/details/Details.jsx
+++ client/src/components/classes/details/Details.jsx
@@ -5,26 +5,29 @@ import DetailsCard from './DetailsCard';
 const baseUrl = `http://localhost:3030/jsonstore`;
 
 const Details = () => {
-  const { id } = useParams();
-  const [card, setCard] = useState(null);
+    const { id } = useParams();
+    const [card, setCard] = useState(null);
 
-  useEffect(() => {
-    (async function fetchCard() {
-      try {
-        const response = await fetch(`${baseUrl}/classes/${id}`);
-        const result = await response.json();
-        setCard(result);
-      } catch (error) {
-        alert(error.message);
-      }
-    })();
-  }, [id]);
+    useEffect(() => {
+        (async function fetchCard() {
+            try {
+                const response = await fetch(`${baseUrl}/classes/${id}`);
+                const result = await response.json();
+                const data = Object.values(result);
+                const cards = Object.values(data[0])
+console.log(cards);
+                setCard(cards);
+            } catch (error) {
+                alert(error.message);
+            }
+        })();
+    }, [id]);
 
-  if (!card) {
-    return <div>Loading...</div>;
-  }
+    if (!card) {
+        return <div>Loading...</div>;
+    }
 
-  return <DetailsCard card={card} />;
+    return <DetailsCard card={card} />;
 };
 
 export default Details;
