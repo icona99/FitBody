5cba1cd6a88fc342acdcee6622bccc35eecefa45
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-07-19 13:35:40 +0300

    add animation to home button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,31 +8,35 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Footer from './components/footer/Footer';
 import Login from './components/login/Login';
-import Register from './components/register/Register'
+import Register from './components/register/Register';
 import About from './components/about/About';
 import Profile from './components/profile/Profile';
 import Classes from './components/classes/Classes';
 import AddClass from './components/addClass/AddClass';
-import Details from './components//details/Details';
+import Details from './components/details/Details';
+// import '../public/styles/styles.css'
 
 function App() {
   return (
-   <div>
+    <div className="container">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/classes' element={<Classes />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/addClass' element={<AddClass />} />
-        <Route path='/details/:id' element={<Details />} />
-      </Routes>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/classes' element={<Classes />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/addClass' element={<AddClass />} />
+          <Route path='/details/:id' element={<Details />} />
+        </Routes>
+      </div>
       <Footer />
-   </div>
+    </div>
   );
 }
 
 export default App;
+
 
