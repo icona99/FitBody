6602f3937b5d40e76939d99c8e7fff55dffedad6
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-07-22 14:20:48 +0300

    add css for editCard component

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -7,38 +7,18 @@ const baseUrl = `http://localhost:3030/jsonstore`;
 const Details = () => {
     const { id } = useParams();
     const [card, setCard] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         (async function fetchCard() {
             try {
                 const response = await fetch(`${baseUrl}/classes/${id}`);
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
                 const result = await response.json();
-                console.log('Fetched Data:', result); 
                 setCard(result); 
             } catch (error) {
-                setError(error.message);
-            } finally {
-                setLoading(false); 
+                alert(error.message);
             }
         })();
     }, [id]);
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>Error: {error}</div>; 
-    }
-
-    if (!card) {
-        return <div>No data found</div>; 
-    }
 
     return <DetailsCard card={card} />;
 };
---
+++ client/src/components/edit/EditCard.jsx
@@ -0,0 +1,26 @@
+import { useParams } from "react-router-dom";
+import DetailsCard from "../cardDetails/DetailsCard";
+import styles from "./Edit.module.css";
+import { useEffect, useState } from "react";
+
+const baseUrl = 'http://localhost:3030/jsonstore'
+
+export default function EditCard() {
+
+    const { id } = useParams();
+    const [card, setCard] = useState(null);
+
+    useEffect (() => {
+        (async function fetchCard() {
+            try {
+                const response = await fetch(`${baseUrl}/classes/${id}`);
+                const result = await response.json();
+                setCard(result)
+            } catch (error) {
+                alert(error.message)
+            }
+
+        })();
+    }, [id])
+    return <DetailsCard card={card} />;
+}
