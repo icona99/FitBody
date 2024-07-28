2a96a9a3bf9063fa5ab1c75b8d48c59182c43d95
Hristo Georgiev <145356615+icona99@users.noreply.github.com>
2024-07-28 13:13:18 +0300

    Add delete card logic and execution

--- client/src/api/classesAPI.js
+++ client/src/api/classesAPI.js
@@ -9,10 +9,13 @@ export const getOne = (classId) => request.get(`${BASE_URL}/${classId}`);
 
 export const create = (classData) => request.post(`${BASE_URL}`, classData);
 
+export const remove = (classId) => request.remove(`${BASE_URL}/${classId}`);
+
 const classesAPI = {
     getAll,
     getOne,
     create,
+    remove
 }
 
 export default classesAPI
--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -1,25 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import classesAPI from '../../api/classesAPI';
 import styles from './Details.module.css';
-import {Link, useParams} from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 
 
 const Details = () => {
-    const [card, setCard] = useState({});
-    const {classId}=useParams();
+  const [card, setCard] = useState({});
+  const { classId } = useParams();
+  const navigate = useNavigate()
 
-    useEffect(() => {
-        (async ()=> {
-            try {
-                const result = await classesAPI.getOne(classId);
-                setCard(result); 
-            } catch (error) {
-                alert(error.message);
-            }
-        })();
-    }, [classId]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await classesAPI.getOne(classId);
+        setCard(result);
+      } catch (error) {
+        alert(error.message);
+      }
+    })();
+  }, [classId]);
 
-    return (<div className={styles.cardContainer}>
+  const handleDelete = async () => {
+    try {
+      await classesAPI.remove(classId);
+      navigate('/classes');
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (<div className={styles.cardContainer}>
     <div className={styles.card}>
       <img src={card.imageUrl} alt={card.title} className={styles.image} />
       <h2>{card.title}</h2>
@@ -27,7 +37,7 @@ const Details = () => {
       <p>{card.description}</p>
       <div className={styles.buttons}>
         <Link to={`/edit/${card._id}`} className={styles.editButton}>Edit</Link>
-        <Link to={`/delete/${card._id}`} className={styles.deleteButton}>Delete</Link>
+        <button onClick={handleDelete} className={styles.deleteButton}>Delete</button>
       </div>
     </div>
   </div>
